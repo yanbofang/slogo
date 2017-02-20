fa06779a36783ed6b2175b3aab48866218131c1c
Gordon Huynh <ghh6@duke.edu>
2017-02-20 15:56:16 -0500

    Frontend APIs added

---
+++ src/frontend/API/VariablesViewAPI.java
@@ -0,0 +1,17 @@
+package frontend.API;
+
+/**
+ * API for the variables display subcomponent
+ * @author Gordon
+ *
+ */
+public interface VariablesViewAPI {
+	/**
+	 * Adds or updates a variable to the list of variables to display
+	 * @param a
+	 * The name of the variable
+	 * @param b
+	 * The value of the variable
+	 */
+	public void updateVar(String a, String b);
+}
