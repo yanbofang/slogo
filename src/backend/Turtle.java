ae7a9f3b0b62ee8bd4aa476fe7ff6a12241026b5
Henry Taylor <hjt8@duke.edu>
2017-02-22 22:37:54 -0500

    fixed parser

--- src/backend/Turtle.java
+++ src/backend/Turtle.java
@@ -7,15 +7,16 @@ import javafx.scene.image.ImageView;
 
 public class Turtle {
 	
-	private final String myTurtlePicture = "turtle.png";
+	private final String myTurtlePicture = "images/turtle.png";
 	
 	private ImageView myImage;
 	
 
 	
 	public Turtle(double width, double height) {
+		//myImage = new ImageView();
 		Image turtleView = new Image(getClass().getClassLoader().getResourceAsStream(myTurtlePicture));
-		myImage.setImage(turtleView);
+		myImage = new ImageView(turtleView);
 		myImage.setFitWidth(width);
 		myImage.setFitHeight(height);
 	}
