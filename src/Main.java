55ee4d2611c8f3c5371213bd1f2c35a75297b261
Yanbo Fang <yf59@duke.edu>
2017-02-27 16:52:55 -0500

    fixed bug in MODEL

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application{
+	
+	private static final String[] SYNTAX = new String[]{"resources/languages/English", "resources/languages/Syntax"};
+	private static Model m = new Model(SYNTAX, null);
 
 	public void start(Stage arg0) throws Exception {
-		Model model = new Model();
 		View view = new View(arg0);
-		view.runView(model);
+		view.runView(m);
 	}
 	
 	public static void main(String[] argv) {
-		String[] syntax = new String[]{"resources/languages/English", "resources/languages/Syntax"};
-		Model m = new Model(syntax, "resources/languages/Commands", null);
 		m.handleInput("sum sum sum 5 sum sum 1 2 4 6 7"
 				+ " sum sum sum 5 6 0 9");
 		//m.handleInput("setxy 5 0");
