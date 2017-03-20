32a80811313da5f15ff91f137aafbff090f01bdf
Henry Taylor <hjt8@duke.edu>
2017-03-20 17:31:07 -0400

    MY MASTERPIECE

--- src/backend/CommandFactory.java
+++ src/backend/CommandFactory.java
@@ -1,11 +1,28 @@
 package backend;
 
+//This entire file is a part of my masterpiece
+//Henry Taylor
+
+/**
+ * The purpose of this class is to reflect a given string to a command class. I think this shows good
+ * design because the only purpose of this class is to reflect and return a command, which isolates its function,
+ * creating a small but very important class.
+ */
 import commands.Command;
 
 public class CommandFactory {
 	
 	private static final String COMMAND_PACKAGE = "commands.";
 	
+	/**
+	 * Uses reflection to find the corresponding command
+	 * @param input - the name of the command before myParser converts it -- used to give the command
+	 * 					the actual name of the instruction
+	 * @param s - the actual command name
+	 * @param variables - variable manager
+	 * @param methods - method manager
+	 * @return returns a new command or null if no match
+	 */
 	public Command reflectCommand(String input, String s, VariableManager variables, UserMethodManager methods) {
 		try {
 			Class<?> clazz = Class.forName( COMMAND_PACKAGE + s + "Command" );
--- src/backend/CommandHandler.java
+++ src/backend/CommandHandler.java
@@ -1,5 +1,6 @@
 package backend;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -7,6 +8,17 @@ import java.util.Queue;
 import turtles.TurtleManager;
 import commands.Command;
 
+
+//This entire file is a part of my masterpiece
+//Henry Taylor 
+
+/**
+ * This class is responsible for handling all of the commands when the controller tells the backend 
+ * to process them. We use a queue to store and process commands in order as they are received.
+ * I think this is good design because we delegate a class to only worry about the execution of commands
+ * @author Henry
+ *
+ */
 public class CommandHandler {
 
 	private Queue<Command> myCommands;
@@ -20,16 +32,21 @@ public class CommandHandler {
 		myVariables = vars;
 	}
 
-	public void addCommands(List<Command> lst) {
+	/**
+	 * Allows model to add more commands to execute
+	 * @param lst - list of Commands to add to myCommands
+	 */
+	public void addCommands(Collection<Command> lst) {
 		for (Command c : lst) {
 			myCommands.add(c);
 		}
 	}
 
 	/**
-	 * Execute a single command -- assures we return our values for each command
-	 * 
-	 * @return
+	 * Execute a single command -- assures we return our values for each command.
+	 * Checks to see if command is a command that can be run by all turtles, if it is, it
+	 * runs the command for all active turtles
+	 * @return value of the executed command
 	 */
 
 	public Double executeCommands() {
